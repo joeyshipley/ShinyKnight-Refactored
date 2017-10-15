5c7d56a524e225e9a81d37e461711f240fe80f2a
Joey Shipley <joey.shipley@gmail.com>
2017-10-15 10:14:53 -0400

    reorganizing the tests

--- tests/shinyknight.tests.js
+++ tests/shinyknight.tests.js
@@ -20,37 +20,29 @@ afterEach(() => {
 });
 
 describe('ShinyKnights Battle Foes', () => {
-  describe('#dmg => When dealing damage to a ShinyKnight', () => {
+  describe('#dmg => When our Young Squire is concerned about how much damage they take', () => {
 
-    describe('and our mighty templar recieved a weak attack,', () => {
+    describe('and they recieved a weak attack,', () => {
       it('it does not apply damage', () => {
         const result = CUT.dmg('unknown', 0, 0, false, OPPONENT);
         expect(result).to.equal('You suffered no damage from the attack, way to go!');
       });
     });
 
-    describe('and our young squire recieved an overpowering attack,', () => {
+    describe('and they recieved an overpowering attack,', () => {
       it('it defeats the ShinyKnight', () => {
         const result = CUT.dmg('unknown', 100, 0, false, OPPONENT);
         expect(result).to.equal('You Sir Joseph have perished. You respawn back at town square but have suffered loss in level. You are now level 1');
       });
     });
 
-    describe('and our epic captain recieves a standard attack,', () => {
-      it('it applies reduced damage', () => {
-        const result = CUT.dmg('standard', 11, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
-      });
-    });
+  });
+});
 
-    describe('and our mystic guardian recieves a magic attack,', () => {
-      it('it applies reduced damage', () => {
-        const result = CUT.dmg('magic', 11, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
-      });
-    });
+describe('ShinyKnights Battle Foes', () => {
+  describe('#dmg => When our Wise Defender is concerned about armor', () => {
 
-    describe('and our friendly warrior was not wearing armor,', () => {
+    describe('and they are not wearing armor,', () => {
       it('it applies damage without armor reduction', () => {
         CUT.armor = null;
         const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
@@ -58,7 +50,7 @@ describe('ShinyKnights Battle Foes', () => {
       });
     });
 
-    describe('and our wise defender was wearing leather armor,', () => {
+    describe('and they are wearing leather armor,', () => {
       it('it applies reduced damage', () => {
         CUT.armor = 'Leather';
         const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
@@ -66,7 +58,58 @@ describe('ShinyKnights Battle Foes', () => {
       });
     });
 
-    describe('and our bright champion did not evade,', () => {
+  });
+});
+
+describe('ShinyKnights Battle Foes', () => {
+  describe('#dmg => When our Perceptive Sentry is concerned about suprise attacks', () => {
+
+    describe('and they were suprised by the attack,', () => {
+      it('it applies damage extra damage', () => {
+        const result = CUT.dmg('unknown', 7, 0, true, OPPONENT);
+        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+      });
+    });
+
+    describe('and they were not suprised by the attack,', () => {
+      it('it does not increase the damage', () => {
+        const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 7 wounds and now have 13 health left');
+      });
+    });
+
+  });
+});
+
+describe('ShinyKnights Battle Foes', () => {
+  describe('#dmg => When our Verstile Champion is concerned about the type of an attack', () => {
+
+    describe('and they recieve a standard attack,', () => {
+      it('it applies reduced damage', () => {
+        const result = CUT.dmg('standard', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
+      });
+    });
+
+    describe('and they recieves a magic attack,', () => {
+      it('it applies reduced damage', () => {
+        const result = CUT.dmg('magic', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+      });
+    });
+
+  });
+});
+
+describe('ShinyKnights Battle Foes', () => {
+  describe('#dmg => When our Sturdy Protector is concerned about the attacks with armor penatration', () => {
+  });
+});
+
+describe('ShinyKnights Battle Foes', () => {
+  describe('#dmg => When our Spry Hero is concerned about evading attacks', () => {
+
+    describe('and they did not evade,', () => {
       it('it applies damage without evade reduction', () => {
         evade_chance.callsFake(() => { return 1; });
 
@@ -75,7 +118,7 @@ describe('ShinyKnights Battle Foes', () => {
       });
     });
 
-    describe('and our peerless hero evaded some of the attack,', () => {
+    describe('and they evaded some of the attack,', () => {
       it('it applies reduced damage', () => {
         evade_chance.callsFake(() => { return 100; });
 
@@ -84,19 +127,10 @@ describe('ShinyKnights Battle Foes', () => {
       });
     });
 
-    describe('and our stalwart protector was suprised by the attack,', () => {
-      it('it applies damage extra damage', () => {
-        const result = CUT.dmg('unknown', 7, 0, true, OPPONENT);
-        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
-      });
-    });
+  });
+});
 
-    describe('and our perceptive sentry was not suprised by the attack,', () => {
-      it('it does not increase the damage', () => {
-        const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 7 wounds and now have 13 health left');
-      });
-    });
-
+describe('ShinyKnights Battle Foes', () => {
+  describe('#dmg => When our Experienced Templar is concerned about what their profession is', () => {
   });
 });
