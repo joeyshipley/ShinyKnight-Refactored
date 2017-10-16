bd2c2e74f39c3826c9e9590103fcee1c8f08539a
Joey Shipley <joey.shipley@gmail.com>
2017-10-16 11:10:10 -0400

    Refactoring magic values first pass

---
+++ src/gameRules.constants.js
@@ -0,0 +1,57 @@
+export const RULES = {
+  CHECK: {
+    BASE_EVADE: 100,
+    MIN_LEVEL: 1,
+    MIN_HEALTH: 0,
+    NO_DAMAGE: 0
+  },
+  MODIFIER: {
+    SURPRISE_DAMAGE: 1.5,
+    EVADE_REDUCTION: 1.25
+  },
+  CHARACTER_CLASS: {
+    KNIGHT: 'Knight',
+    WIZARD: 'Wizard',
+    SHINOBI: 'Shinobi'
+  },
+  RESIST_TYPE_BASE_VALUE: {
+    UNKNOWN: 0,
+    STANDARD: 0,
+    MAGIC: 1,
+    EARTH: 1,
+    FIRE: 1,
+    WATER: 1,
+    WIND: 1,
+    SHADOW: 1,
+    ICE: 1,
+    LIGHTNING: 1,
+    DARK: 1,
+    LIGHT: 1,
+    PSIONIC: 1
+  },
+  DAMAGE_TYPE: {
+    STANDARD: 'standard',
+    MAGIC: 'magic',
+    EARTH: 'earth',
+    FIRE: 'fire',
+    WATER: 'water',
+    WIND: 'wind',
+    SHADOW: 'shadow',
+    ICE: 'ice',
+    LIGHTNING: 'lightning',
+    DARK: 'dark',
+    LIGHT: 'light',
+    PSIONIC: 'psionic'
+  },
+  ARMOR: {
+    LEATHER: 'Leather',
+    LEATHER_DEFENSE: 2,
+    CHAIN_MAIL: 'Chain mail',
+    CHAIN_MAIL_DEFENSE: 6,
+    FULL_PLATE: 'Full Plate',
+    FULL_PLATE_DEFENSE: 12
+  },
+  WEAPON: {
+    LONGSWORD: 'Longsword'
+  }
+}
--- src/shinyknight.js
+++ src/shinyknight.js
@@ -1,16 +1,17 @@
 import { MATH } from './util/math.utils.js';
+import { RULES } from './gameRules.constants';
 
 export class ShinyKnight {
 
   constructor() {
     this.c_name = 'Sir Joseph';
-    this.c_class = 'Knight';
+    this.c_class = RULES.CHARACTER_CLASS.KNIGHT;
     this.c_lvl = 2;
     this.c_hp = 20;
     this.c_evade = 5 // 5% chance to reduce damage after resistances/def are calculated by 1/2;
     this.c_counter = 2 // 2% chance to counter an attack;
-    this.armor = 'Leather';
-    this.weapon = 'Longsword';
+    this.armor = RULES.ARMOR.LEATHER;
+    this.weapon = RULES.WEAPON.LONGSWORD;
   }
 
   determine_damage_amount_and_apply_damage_from_attack_whether_suprise_or_not_and_possibly_counter_attack(
@@ -29,75 +30,75 @@ export class ShinyKnight {
 
     // surprise attacks do more damage
     if(srpe_att) {
-      d = d * 1.5
+      d = d * RULES.MODIFIER.SURPRISE_DAMAGE;
     }
 
     // when you see it coming, there is a chance that you get to evade part of the damage
-    if(!srpe_att && (MATH.evade_chance() > 100 - this.c_evade)) {
-      d = d / 1.25; // consider redesigning this feature, the original value was way to strong, temp reducing the divider
+    if(!srpe_att && (MATH.evade_chance() > RULES.CHECK.BASE_EVADE - this.c_evade)) {
+      d = d / RULES.MODIFIER.EVADE_REDUCTION; // consider redesigning this feature, the original value was way to strong, temp reducing the divider
     }
 
     // modify damage based on your resistances to the different types
     switch(d_type) {
-      case 'standard':
-        d -= this.c_class == 'Knight' ? 10 : 0; // Warriors get a special resistance to standard damage
+      case RULES.DAMAGE_TYPE.STANDARD:
+        d -= this.c_class == RULES.CHARACTER_CLASS.KNIGHT ? 10 : RULES.RESIST_TYPE_BASE_VALUE.STANDARD; // Warriors get a special resistance to standard damage
         break;
-      case 'magic':
-        d -= this.c_class == 'Wizard' ? 10 : 1; // Magi get a special resistance to magic damage
+      case RULES.DAMAGE_TYPE.MAGIC:
+        d -= this.c_class == RULES.CHARACTER_CLASS.WIZARD ? 10 : RULES.RESIST_TYPE_BASE_VALUE.MAGIC; // Magi get a special resistance to magic damage
         break;
-      case 'earth':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.EARTH:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.EARTH;
         break;
-      case 'fire':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.FIRE:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.FIRE;
         break;
-      case 'water':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.WATER:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.WATER;
         break;
-      case 'wind':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.WIND:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.WIND;
         break;
-      case 'shadow':
-        d -= this.c_class == 'Shinobi' ? 10 : 1; // Rogue get a special resistance to shadow damage
+      case RULES.DAMAGE_TYPE.SHADOW:
+        d -= this.c_class == RULES.CHARACTER_CLASS.SHINOBI ? 10 : RULES.RESIST_TYPE_BASE_VALUE.SHADOW; // Rogue get a special resistance to shadow damage
         break;
-      case 'ice':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.ICE:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.ICE;
         break;
-      case 'lightning':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.LIGHTNING:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.LIGHTNING;
         break;
-      case 'dark':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.DARK:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.DARK;
         break;
-      case 'light':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.LIGHT:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.LIGHT;
         break;
-      case 'psionic':
-        d -= 1;
+      case RULES.DAMAGE_TYPE.PSIONIC:
+        d -= RULES.RESIST_TYPE_BASE_VALUE.PSIONIC;
         break;
       default:
-        d -= 0;
+        d -= RULES.RESIST_TYPE_BASE_VALUE.UNKNOWN;
         break;
     }
 
     // modify damage based on armor worn
-    if(this.armor == 'Leather') {
+    if(this.armor == RULES.ARMOR.LEATHER) {
       if(!srpe_att) { // characters don't get to use armor values when surprise attacked
-        d -= (2 - ap);
+        d -= (RULES.ARMOR.LEATHER_DEFENSE - ap);
       }
-    } else if(this.armor == 'Chain mail') {
+    } else if(this.armor == RULES.ARMOR.CHAIN_MAIL) {
       if(!srpe_att) { // characters don't get to use armor values when surprise attacked
-        d -= (6 - ap);
+        d -= (RULES.ARMOR.CHAIN_MAIL_DEFENSE - ap);
       }
-    } else if(this.armor == 'Full Plate') {
+    } else if(this.armor == RULES.ARMOR.FULL_PLATE) {
       if(!srpe_att) { // characters don't get to use armor values when surprise attacked
-        d -= (12 - ap);
+        d -= (RULES.ARMOR.FULL_PLATE_DEFENSE - ap);
       }
     }
 
     // make sure we don't give them hp when they block it
-    if(d < 0) {
-      d = 0;
+    if(d < RULES.CHECK.MIN_HEALTH) {
+      d = RULES.CHECK.MIN_HEALTH;
     }
 
     d = Math.floor(d);
@@ -108,37 +109,37 @@ export class ShinyKnight {
     // NOTE: this is becoming to painful, removing until we figure out
     // how to handle all the different combos for the counter attack
     // if this.c_hp > 0 && !srpe_att && (rand(100) + 1 < this.c_counter)
-    //   if(this.c_class == 'Knight' && this.weapon == 'Short Sword') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.KNIGHT && this.weapon == 'Short Sword') {
     //     char_att.dmg('physical', rand(10) + 2, 0, false)
     //   }
-    //   if(this.c_class == 'Knight' && this.weapon == 'Longsword') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.KNIGHT && this.weapon == 'Longsword') {
     //     char_att.dmg('physical', rand(20) + 2, 0, false)
     //   }
-    //   if(this.c_class == 'Knight' && this.weapon == 'Battle Axe') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.KNIGHT && this.weapon == 'Battle Axe') {
     //     char_att.dmg('physical', rand(11) + 10, 0, false)
     //   }
-    //   if(this.c_class == 'Wizard' && this.weapon == 'Fireball') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.WIZARD && this.weapon == 'Fireball') {
     //     char_att.dmg('fire', rand(10) + 6, 0, false)
     //   }
-    //   if(this.c_class == 'Wizard' && this.weapon == 'Ice Spikes') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.WIZARD && this.weapon == 'Ice Spikes') {
     //     char_att.dmg('ice', rand(10) + 6, 0, false)
     //   }
-    //   if(this.c_class == 'Wizard' && this.weapon == 'Crushing Grasp') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.WIZARD && this.weapon == 'Crushing Grasp') {
     //     char_att.dmg('magic', rand(10) + 6, 0, false)
     //   }
-    //   if(this.c_class == 'Shinobi' && this.weapon == 'Tanto') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.SHINOBI && this.weapon == 'Tanto') {
     //     char_att.dmg('magic', rand(6) + 6, 6, false)
     //   }
-    //   if(this.c_class == 'Shinobi' && this.weapon == 'Ninjato') {
+    //   if(this.c_class == RULES.CHARACTER_CLASS.SHINOBI && this.weapon == 'Ninjato') {
     //     char_att.dmg('magic', rand(12) + 6, 3, false)
     //   }
     // }
 
     // display results
-    if(d == 0) {
+    if(d == RULES.CHECK.NO_DAMAGE) {
       return 'You suffered no damage from the attack, way to go!';
-    } else if(this.c_hp <= 0) {
-      if(this.c_lvl > 1) { this.c_lvl -= 1 }
+    } else if(this.c_hp <= RULES.CHECK.MIN_HEALTH) {
+      if(this.c_lvl > RULES.CHECK.MIN_LEVEL) { this.c_lvl -= 1 }
       this.c_hp = 20;
       return `You ${ this.c_name } have perished. You respawn back at town square but have suffered loss in level. You are now level ${ this.c_lvl }`;
     } else {
