00da6f284a82a558e14b45179eddf8272799c9d6
Joey Shipley <joey.shipley@gmail.com>
2017-10-15 21:22:25 -0400

    Fleshing out tests. Adds HP reset.

--- tests/shinyknight.tests.js
+++ tests/shinyknight.tests.js
@@ -34,6 +34,16 @@ describe('ShinyKnights Battle Foes', () => {
         const result = CUT.dmg('unknown', 100, 0, false, OPPONENT);
         expect(result).to.equal('You Sir Joseph have perished. You respawn back at town square but have suffered loss in level. You are now level 1');
       });
+
+      it('it reduces the character level by 1', () => {
+        const result = CUT.dmg('unknown', 100, 0, false, OPPONENT);
+        expect(CUT.c_lvl).to.equal(1);
+      });
+
+      it('it resets the hit points to 20', () => {
+        const result = CUT.dmg('unknown', 100, 0, false, OPPONENT);
+        expect(CUT.c_hp).to.equal(20);
+      });
     });
 
   });
@@ -45,8 +55,14 @@ describe('ShinyKnights Battle Foes', () => {
     describe('and they are not wearing armor,', () => {
       it('it applies damage without armor reduction', () => {
         CUT.armor = null;
-        const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 7 wounds and now have 13 health left');
+        const result = CUT.dmg('unknown', 8, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 8 wounds and now have 12 health left');
+      });
+
+      it('it does not take armor penatration into account', () => {
+        CUT.armor = 'Leather';
+        const result = CUT.dmg('unknown', 8, 2, false, OPPONENT);
+        expect(result).to.equal('You have suffered 8 wounds and now have 12 health left');
       });
     });
 
@@ -60,6 +76,12 @@ describe('ShinyKnights Battle Foes', () => {
       it('it does not protect when suprised', () => {
         CUT.armor = 'Leather';
         const result = CUT.dmg('unknown', 10, 0, true, OPPONENT);
+        expect(result).to.equal('You have suffered 15 wounds and now have 5 health left');
+      });
+
+      it('it\'s effectiveness is reduced from armor penatration', () => {
+        CUT.armor = 'Leather';
+        const result = CUT.dmg('unknown', 15, 2, false, OPPONENT);
         expect(result).to.equal('You have suffered 15 wounds and now have 5 health left');
       });
     });
@@ -76,6 +98,12 @@ describe('ShinyKnights Battle Foes', () => {
         const result = CUT.dmg('unknown', 10, 0, true, OPPONENT);
         expect(result).to.equal('You have suffered 15 wounds and now have 5 health left');
       });
+
+      it('it\'s effectiveness is reduced from armor penatration', () => {
+        CUT.armor = 'Chain mail';
+        const result = CUT.dmg('unknown', 15, 2, false, OPPONENT);
+        expect(result).to.equal('You have suffered 11 wounds and now have 9 health left');
+      });
     });
 
     describe('and they are wearing full plate,', () => {
@@ -90,6 +118,12 @@ describe('ShinyKnights Battle Foes', () => {
         const result = CUT.dmg('unknown', 10, 0, true, OPPONENT);
         expect(result).to.equal('You have suffered 15 wounds and now have 5 health left');
       });
+
+      it('it\'s effectiveness is reduced from armor penatration', () => {
+        CUT.armor = 'Full Plate';
+        const result = CUT.dmg('unknown', 15, 2, false, OPPONENT);
+        expect(result).to.equal('You have suffered 5 wounds and now have 15 health left');
+      });
     });
 
   });
@@ -100,15 +134,15 @@ describe('ShinyKnights Battle Foes', () => {
 
     describe('and they were suprised by the attack,', () => {
       it('it applies damage extra damage', () => {
-        const result = CUT.dmg('unknown', 7, 0, true, OPPONENT);
-        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+        const result = CUT.dmg('unknown', 8, 0, true, OPPONENT);
+        expect(result).to.equal('You have suffered 12 wounds and now have 8 health left');
       });
     });
 
     describe('and they were not suprised by the attack,', () => {
       it('it does not increase the damage', () => {
-        const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 7 wounds and now have 13 health left');
+        const result = CUT.dmg('unknown', 8, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 8 wounds and now have 12 health left');
       });
     });
 
@@ -118,25 +152,246 @@ describe('ShinyKnights Battle Foes', () => {
 describe('ShinyKnights Battle Foes', () => {
   describe('#dmg => When our Verstile Champion is concerned about the type of an attack', () => {
 
-    describe('and they recieve a standard attack,', () => {
-      it('it applies reduced damage', () => {
+    describe('and they are a Knights reciving an attack,', () => {
+      it('it\'s standard attack reduced damage', () => {
+        CUT.c_class = 'Knight';
         const result = CUT.dmg('standard', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
+      });
+
+      it('it\'s magic attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('magic', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+      });
+
+      it('it\'s shadow attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('shadow', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+      });
+
+      it('it\'s earth attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('earth', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s fire attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('fire', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s water attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('water', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s wind attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('wind', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s ice attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('ice', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s lightning attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('lightning', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s dark attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('dark', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s light attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('light', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s psionic attack reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('psionic', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('unspecified attack does not reduced damage', () => {
+        CUT.c_class = 'Knight';
+        const result = CUT.dmg('unknown', 1, 0, false, OPPONENT);
         expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
       });
     });
 
-    describe('and they recieves a magic attack,', () => {
-      it('it applies reduced damage', () => {
+    describe('and they are a Wizard reciving an attack,', () => {
+      it('it\'s standard attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('standard', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 11 wounds and now have 9 health left');
+      });
+
+      it('it\'s magic attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
         const result = CUT.dmg('magic', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
+      });
+
+      it('it\'s shadow attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('shadow', 11, 0, false, OPPONENT);
         expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+      });
+
+      it('it\'s earth attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('earth', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s fire attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('fire', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s water attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('water', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s wind attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('wind', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s ice attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('ice', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s lightning attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('lightning', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s dark attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('dark', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s light attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('light', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s psionic attack reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('psionic', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('unspecified attack does not reduced damage', () => {
+        CUT.c_class = 'Wizard';
+        const result = CUT.dmg('unknown', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
       });
     });
 
-  });
-});
+    describe('and they are a Shinobi reciving an attack,', () => {
+      it('it\'s standard attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('standard', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 11 wounds and now have 9 health left');
+      });
 
-describe('ShinyKnights Battle Foes', () => {
-  describe('#dmg => When our Sturdy Protector is concerned about the attacks with armor penatration', () => {
+      it('it\'s magic attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('magic', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 10 wounds and now have 10 health left');
+      });
+
+      it('it\'s shadow attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('shadow', 11, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
+      });
+
+      it('it\'s earth attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('earth', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s fire attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('fire', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s water attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('water', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s wind attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('wind', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s ice attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('ice', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s lightning attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('lightning', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s dark attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('dark', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s light attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('light', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('it\'s psionic attack reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('psionic', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You suffered no damage from the attack, way to go!');
+      });
+
+      it('unspecified attack does not reduced damage', () => {
+        CUT.c_class = 'Shinobi';
+        const result = CUT.dmg('unknown', 1, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 1 wounds and now have 19 health left');
+      });
+    });
+
   });
 });
 
@@ -147,8 +402,8 @@ describe('ShinyKnights Battle Foes', () => {
       it('it applies damage without evade reduction', () => {
         evade_chance.callsFake(() => { return 1; });
 
-        const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 7 wounds and now have 13 health left');
+        const result = CUT.dmg('unknown', 8, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 8 wounds and now have 12 health left');
       });
     });
 
@@ -156,8 +411,17 @@ describe('ShinyKnights Battle Foes', () => {
       it('it applies reduced damage', () => {
         evade_chance.callsFake(() => { return 100; });
 
-        const result = CUT.dmg('unknown', 7, 0, false, OPPONENT);
-        expect(result).to.equal('You have suffered 5 wounds and now have 15 health left');
+        const result = CUT.dmg('unknown', 8, 0, false, OPPONENT);
+        expect(result).to.equal('You have suffered 6 wounds and now have 14 health left');
+      });
+    });
+
+    describe('and they would have evaded but it was a suprise attack,', () => {
+      it('it applies damage without evade reduction', () => {
+        evade_chance.callsFake(() => { return 100; });
+
+        const result = CUT.dmg('unknown', 8, 0, true, OPPONENT);
+        expect(result).to.equal('You have suffered 12 wounds and now have 8 health left');
       });
     });
 
